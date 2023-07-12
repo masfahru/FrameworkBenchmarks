28f3bfd1f756a5b7da94efd0bcd080d201472670
Imam Fahrur Rofi <92277627+masfahru@users.noreply.github.com>
2023-07-12 14:26:05 +0700

    [HyperExpress] Update postgres.js - warm up db script

--- frameworks/JavaScript/hyperexpress/database/postgres.js
+++ frameworks/JavaScript/hyperexpress/database/postgres.js
@@ -11,4 +11,4 @@ export const getAllWorlds = async () => sql`SELECT * FROM world`
 
 export const update = async (obj) => sql`UPDATE world SET randomNumber = ${obj.randomNumber} WHERE id = ${obj.id}`
 
-await Promise.all([...Array(maxConnections).keys()].map(fortunes))
+await Promise.all([...Array(150).keys()].map(fortunes))
