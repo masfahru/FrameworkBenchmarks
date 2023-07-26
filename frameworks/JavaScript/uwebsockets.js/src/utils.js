95e60af3e2918983f45851c4b9993f5fc8a2b968
Imam Fahrur Rofi <92277627+masfahru@users.noreply.github.com>
2023-07-26 22:08:12 +0700

    Update utils.js

--- frameworks/JavaScript/uwebsockets.js/src/utils.js
+++ frameworks/JavaScript/uwebsockets.js/src/utils.js
@@ -3,7 +3,7 @@
  *
  * Add HTTP response headers `Server` which is required by the test suite.
  * Header `Date` is automatically added by uWebsockets
- * https://github.com/uNetworking/uWebSockets/blob/7568327834020f2ceed3ce2a823f23d0cc18cb8c/src/HttpResponse.h#L78
+ * https://github.com/uNetworking/uWebSockets/blob/master/src/HttpResponse.h#L78
  * 
  * https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview
  *
